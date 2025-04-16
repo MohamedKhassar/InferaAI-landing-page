2ff479eb7e7a9e14b7a3c3cf8e8a9123d002d7ad
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-04-16 18:05:07 +0100

    update the Navbar component by removing border and change border radius from 3xl to xl

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Navbar from "./components/layout/Navbar"
 
 const App = () => {
   return (
-    <div className="">
+    <div className="relative">
       <Navbar />
+      {/* blur div */}
+      {/* <div className="w-44 h-44 bg-gradient-to-r rotate-45 from-blue-500 via-purple-600 to-transparent blur-2xl rounded-full"></div> */}
     </div>
   )
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -27,14 +27,14 @@ const Navbar = () => {
         }
     })
     return (
-        <header className="lg:py-5 lg:px-6 py-3 px-4 bg-black/30 backdrop-blur-md sticky top-4 border-purple-600 border-2 lg:rounded-4xl rounded-2xl mt-4 z-50 flex items-center justify-between mx-5">
+        <header className="lg:py-5 lg:px-6 py-3 px-4 bg-black/30 backdrop-blur-md sticky top-4 lg:rounded-4xl rounded-2xl mt-4 z-50 flex items-center justify-between mx-5">
             <div>
                 <a href="" className="flex items-center gap-3 w-fit">
                     <img src={logo} className="lg:size-12 size-10" alt="" />
                     <h1 className="lg:text-2xl text-lg font-semibold bg-gradient-to-r from-purple-500 via-purple-500 to-cyan-500 text-transparent bg-clip-text">InferaAI</h1>
                 </a>
             </div>
-            <nav ref={navRef} className={`flex lg:flex-row justify-between items-center lg:max-w-3xl lg:w-full w-fit gap-6 lg:static absolute top-20 right-3 lg:border-none border-violet-500 border-2 rounded-3xl lg:p-0 px-4 py-6 ${!isOpen ? "hidden lg:flex" : "flex-col bg-black/30 backdrop-blur-3xl"}`}>
+            <nav ref={navRef} className={`flex lg:flex-row justify-between items-center lg:max-w-3xl lg:w-full w-fit gap-6 lg:static absolute top-20 right-3 lg:border-none border-violet-500 border-2 rounded-xl lg:p-0 px-4 py-6 ${!isOpen ? "hidden lg:flex" : "flex-col bg-black/30 backdrop-blur-3xl"}`}>
                 <ul className="flex flex-col lg:flex-row items-center gap-5">
                     {navLinks.map(({ text, href }, index) => (
                         <NavItem key={index} text={text} href={href} />
