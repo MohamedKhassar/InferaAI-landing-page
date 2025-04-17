b91497b3be6a036668ea6501383624ec0ea2536d
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-04-17 01:16:34 +0100

    update Hero component for improved responsiveness and add AI context to heading; adjust body max-width in CSS

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -5,17 +5,17 @@ const Hero = () => {
     return (
         <div className="flex lg:flex-row flex-col justify-between items-center lg:my-16 my-10 lg:mx-5 mx-3 lg:gap-0 gap-18">
             <div className="max-w-[44rem] space-y-7">
-                <h1 className="lg:text-6xl lg:text-start text-center text-xl font-extrabold lg:leading-20">
+                <h1 className="lg:text-6xl md:text-3xl lg:text-start text-center text-xl font-extrabold lg:leading-20">
                     Empower Your Business
                     <span className="bg-gradient-to-r from-blue-600 to-purple-700 text-transparent bg-clip-text"> with AI</span>
                 </h1>
-                <p className="lg:text-lg text-sm text-gray-300 lg:text-start text-center lg:leading-8 leading-7">
+                <p className="lg:text-lg md:text-base text-sm text-gray-300 lg:text-start text-center lg:leading-8 leading-7">
                     Our AI SaaS platform seamlessly integrates with your existing workflows to deliver real‑time insights, intelligent automation, and data‑driven decision-making. Experience a future where your business runs smarter, faster, and more efficiently.
                 </p>
                 <div className="flex items-center border border-slate-700 px-3 rounded-full bg-slate-900 lg:w-4/5 w-full place-self-center lg:place-self-start gap-1">
-                    <MailOpen className="lg:mx-4 mx-2 lg:size-8 text-gray-600" />
-                    <input type="email" className="outline-none h-full lg:w-full w-4/6" required placeholder="johndoe@gmail.com" />
-                    <Button className="rounded-full my-2">
+                    <MailOpen className="md:mx-4 mx-2 md:size-8 text-gray-600" />
+                    <input type="email" className="outline-none h-full md:w-full w-4/6 lg:placeholder:text-base placeholder:text-sm" required placeholder="johndoe@gmail.com" />
+                    <Button className="rounded-full my-2 md:text-base text-xs !py-3 !px-4">
                         Subscribe
                     </Button>
                 </div>
