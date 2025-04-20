f72e663b6a7f5963e0a8af1ee17ea7ffa04a658f
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-04-20 02:30:23 +0100

    fix: add w-full class to Card component for responsive width adjustment

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -9,7 +9,7 @@ const Card = ({ Icon, ...feature }: FeatureItem) => {
       initial="initial"
       whileInView="animate"
       viewport={{ once: true }}
-      className="lg:w-120 md:w-93 bg-gradient-to-r from-purple-900 via-violet-900 to-cyan-900 rounded-2xl p-1">
+      className="lg:w-120 md:w-93 w-full bg-gradient-to-r from-purple-900 via-violet-900 to-cyan-900 rounded-2xl p-1">
       <div className="size-full bg-slate-950 rounded-xl flex flex-col justify-center items-center md:space-y-5 space-y-3 p-5">
         <Icon className="bg-gray-900 hover:bg-gray-900/10 duration-300 cursor-pointer hover:border-slate-900 border-2 border-slate-700 p-5 size-max rounded-3xl" />
         <h3 className="lg:text-xl md:text-lg text-base font-extrabold">{feature.title}</h3>
